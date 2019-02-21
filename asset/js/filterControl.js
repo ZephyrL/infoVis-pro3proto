a3a62c63629d09e5836159b10f36007efcba3236
ZephyrL <3150104024@zju.edu.cn>
2019-02-21 11:24:25 +0100

    branch init

--- asset/js/filterControl.js
+++ asset/js/filterControl.js
@@ -1,2 +1,19 @@
 {
+    $("#energy").click(function (event){
+        alert("energy button clicked");
+    })
+
+    $("#green").click(function (event){
+
+    })
+
+    $("#tranports").click(function (event){
+
+    })
+
+    $("#waste").click(function (event){
+        
+    })
+
+
 }
