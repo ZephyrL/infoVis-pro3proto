ce4c071436471a680cc13fd13b19aa7563fb2494
ZephyrL <3150104024@zju.edu.cn>
2019-03-06 18:36:31 +0100

    clean project

--- asset/data/building.js
+++ asset/data/building.js
@@ -1,317 +1,317 @@
 var building = [
 	{
-	  "building": "Abisko 5",
-	  "developer": "Reinhold Gustafsson",
-	  "phase": "Norra 1",
-	  "lat": 59.35595361,
-	  "lng": 18.0868621
+		"building": "Abisko 5",
+		"developer": "Reinhold Gustafsson",
+		"phase": "Norra 1",
+		"lat": 59.35595361,
+		"lng": 18.0868621
 	},
 	{
-	  "building": "Abisko 4",
-	  "developer": "Lennart Ericsson",
-	  "phase": "Norra 1",
-	  "lat": 59.3560205,
-	  "lng": 18.08616256
+		"building": "Abisko 4",
+		"developer": "Lennart Ericsson",
+		"phase": "Norra 1",
+		"lat": 59.3560205,
+		"lng": 18.08616256
 	},
 	{
-	  "building": "Abisko 2",
-	  "developer": "Erik Wallin",
-	  "phase": "Norra 1",
-	  "lat": 59.35637207,
-	  "lng": 18.08663813
+		"building": "Abisko 2",
+		"developer": "Erik Wallin",
+		"phase": "Norra 1",
+		"lat": 59.35637207,
+		"lng": 18.08663813
 	},
 	{
-	  "building": "Abisko 1",
-	  "developer": "Jarntorget",
-	  "phase": "Norra 1",
-	  "lat": 59.35670083,
-	  "lng": 18.08630493
+		"building": "Abisko 1",
+		"developer": "Jarntorget",
+		"phase": "Norra 1",
+		"lat": 59.35670083,
+		"lng": 18.08630493
 	},
 	{
-	  "building": "Abisko 3",
-	  "developer": "Viktor Hanson",
-	  "phase": "Norra 1",
-	  "lat": 59.35634397,
-	  "lng": 18.0856887
+		"building": "Abisko 3",
+		"developer": "Viktor Hanson",
+		"phase": "Norra 1",
+		"lat": 59.35634397,
+		"lng": 18.0856887
 	},
 	{
-	  "building": "Garphyttan 3",
-	  "developer": "Seniorgarden",
-	  "phase": "Norra 1",
-	  "lat": 59.35659209,
-	  "lng": 18.08513025
+		"building": "Garphyttan 3",
+		"developer": "Seniorgarden",
+		"phase": "Norra 1",
+		"lat": 59.35659209,
+		"lng": 18.08513025
 	},
 	{
-	  "building": "Garphyttan 1; 2",
-	  "developer": "NCC",
-	  "phase": "Norra 1",
-	  "lat": 59.35711503,
-	  "lng": 18.08572756
+		"building": "Garphyttan 1; 2",
+		"developer": "NCC",
+		"phase": "Norra 1",
+		"lat": 59.35711503,
+		"lng": 18.08572756
 	},
 	{
-	  "building": "Gotska sandon 2",
-	  "developer": "Familjebostader",
-	  "phase": "Norra 1",
-	  "lat": 59.35653812,
-	  "lng": 18.08799931
+		"building": "Gotska sandon 2",
+		"developer": "Familjebostader",
+		"phase": "Norra 1",
+		"lat": 59.35653812,
+		"lng": 18.08799931
 	},
 	{
-	  "building": "Gotska sandon 1",
-	  "developer": "SBC Bo",
-	  "phase": "Norra 1",
-	  "lat": 59.35661945,
-	  "lng": 18.08719783
+		"building": "Gotska sandon 1",
+		"developer": "SBC Bo",
+		"phase": "Norra 1",
+		"lat": 59.35661945,
+		"lng": 18.08719783
 	},
 	{
-	  "building": "Skuleskogen 2",
-	  "developer": "Svenska Hus 1",
-	  "phase": "Norra 1",
-	  "lat": 59.35785655,
-	  "lng": 18.08700585
+		"building": "Skuleskogen 2",
+		"developer": "Svenska Hus 1",
+		"phase": "Norra 1",
+		"lat": 59.35785655,
+		"lng": 18.08700585
 	},
 	{
-	  "building": "Tyresta 1",
-	  "developer": "Boratt",
-	  "phase": "Vastra",
-	  "lat": 59.35425839,
-	  "lng": 18.08886537
+		"building": "Tyresta 1",
+		"developer": "Boratt",
+		"phase": "Vastra",
+		"lat": 59.35425839,
+		"lng": 18.08886537
 	},
 	{
-	  "building": "Tyresta 2",
-	  "developer": "JM (Tyresta)",
-	  "phase": "Vastra",
-	  "lat": 59.35384872,
-	  "lng": 18.08934773
+		"building": "Tyresta 2",
+		"developer": "JM (Tyresta)",
+		"phase": "Vastra",
+		"lat": 59.35384872,
+		"lng": 18.08934773
 	},
 	{
-	  "building": "Kosterhavet 1; 2",
-	  "developer": "ByggVesta",
-	  "phase": "Vastra",
-	  "lat": 59.3533684,
-	  "lng": 18.08945898
+		"building": "Kosterhavet 1; 2",
+		"developer": "ByggVesta",
+		"phase": "Vastra",
+		"lat": 59.3533684,
+		"lng": 18.08945898
 	},
 	{
-	  "building": "Farnebofjarden 1",
-	  "developer": "Einar Mattsson",
-	  "phase": "Vastra",
-	  "lat": 59.35339862,
-	  "lng": 18.08725989
+		"building": "Farnebofjarden 1",
+		"developer": "Einar Mattsson",
+		"phase": "Vastra",
+		"lat": 59.35339862,
+		"lng": 18.08725989
 	},
 	{
-	  "building": "Farnebofjarden 2",
-	  "developer": "Jarntorget",
-	  "phase": "Vastra",
-	  "lat": 59.35366478,
-	  "lng": 18.08658025
+		"building": "Farnebofjarden 2",
+		"developer": "Jarntorget",
+		"phase": "Vastra",
+		"lat": 59.35366478,
+		"lng": 18.08658025
 	},
 	{
-	  "building": "Tresticklan 2",
-	  "developer": "HSB",
-	  "phase": "Vastra",
-	  "lat": 59.35398864,
-	  "lng": 18.08686938
+		"building": "Tresticklan 2",
+		"developer": "HSB",
+		"phase": "Vastra",
+		"lat": 59.35398864,
+		"lng": 18.08686938
 	},
 	{
-	  "building": "Tresticklan 1",
-	  "developer": "JM (Tresticklan)",
-	  "phase": "Vastra",
-	  "lat": 59.35432139,
-	  "lng": 18.08729039
+		"building": "Tresticklan 1",
+		"developer": "JM (Tresticklan)",
+		"phase": "Vastra",
+		"lat": 59.35432139,
+		"lng": 18.08729039
 	},
 	{
-	  "building": "Norra Kvill 1; Padjelanta 1; 2",
-	  "developer": "Primula",
-	  "phase": "Vastra",
-	  "lat": 59.35449566,
-	  "lng": 18.08974146
+		"building": "Norra Kvill 1; Padjelanta 1; 2",
+		"developer": "Primula",
+		"phase": "Vastra",
+		"lat": 59.35449566,
+		"lng": 18.08974146
 	},
 	{
-	  "building": "Muddus 1",
-	  "developer": "SKB",
-	  "phase": "Vastra",
-	  "lat": 59.35344559,
-	  "lng": 18.0881548
+		"building": "Muddus 1",
+		"developer": "SKB",
+		"phase": "Vastra",
+		"lat": 59.35344559,
+		"lng": 18.0881548
 	},
 	{
-	  "building": "Tofsingdalen 1",
-	  "developer": "Stockholmshem",
-	  "phase": "Vastra",
-	  "lat": 59.35472445,
-	  "lng": 18.08860464
+		"building": "Tofsingdalen 1",
+		"developer": "Stockholmshem",
+		"phase": "Vastra",
+		"lat": 59.35472445,
+		"lng": 18.08860464
 	},
 	{
-	  "building": "Bjornlandet 1",
-	  "developer": "Svenska Bostader (Bjornlandet)",
-	  "phase": "Vastra",
-	  "lat": 59.35575231,
-	  "lng": 18.08854227
+		"building": "Bjornlandet 1",
+		"developer": "Svenska Bostader (Bjornlandet)",
+		"phase": "Vastra",
+		"lat": 59.35575231,
+		"lng": 18.08854227
 	},
 	{
-	  "building": "Soderasen 1",
-	  "developer": "Svenska Bostader (Soderasen)",
-	  "phase": "Vastra",
-	  "lat": 59.3547442,
-	  "lng": 18.08727213
+		"building": "Soderasen 1",
+		"developer": "Svenska Bostader (Soderasen)",
+		"phase": "Vastra",
+		"lat": 59.3547442,
+		"lng": 18.08727213
 	},
 	{
-	  "building": "Stora Sjofallet 1",
-	  "developer": "Bonava",
-	  "phase": "Norra 2",
-	  "lat": 59.35820789,
-	  "lng": 18.0878032
+		"building": "Stora Sjofallet 1",
+		"developer": "Bonava",
+		"phase": "Norra 2",
+		"lat": 59.35820789,
+		"lng": 18.0878032
 	},
 	{
-	  "building": "Stora Sjofallet 4",
-	  "developer": "SSM",
-	  "phase": "Norra 2",
-	  "lat": 59.35830258,
-	  "lng": 18.08849265
+		"building": "Stora Sjofallet 4",
+		"developer": "SSM",
+		"phase": "Norra 2",
+		"lat": 59.35830258,
+		"lng": 18.08849265
 	},
 	{
-	  "building": "Stora Sjofallet 2",
-	  "developer": "Viktor Hanson",
-	  "phase": "Norra 2",
-	  "lat": 59.35866446,
-	  "lng": 18.08830723
+		"building": "Stora Sjofallet 2",
+		"developer": "Viktor Hanson",
+		"phase": "Norra 2",
+		"lat": 59.35866446,
+		"lng": 18.08830723
 	},
 	{
-	  "building": "Stora Sjofallet 3",
-	  "developer": "Wallenstam",
-	  "phase": "Norra 2",
-	  "lat": 59.35865841,
-	  "lng": 18.08928653
+		"building": "Stora Sjofallet 3",
+		"developer": "Wallenstam",
+		"phase": "Norra 2",
+		"lat": 59.35865841,
+		"lng": 18.08928653
 	},
 	{
-	  "building": "Domarudden 2",
-	  "developer": "Wallenstam forskola",
-	  "phase": "Norra 2",
-	  "lat": 59.35828293,
-	  "lng": 18.08965828
+		"building": "Domarudden 2",
+		"developer": "Wallenstam forskola",
+		"phase": "Norra 2",
+		"lat": 59.35828293,
+		"lng": 18.08965828
 	},
 	{
-	  "building": "Hornslandet 1",
-	  "developer": "Stockholmshem",
-	  "phase": "Norra 2",
-	  "lat": 59.35886237,
-	  "lng": 18.09030331
+		"building": "Hornslandet 1",
+		"developer": "Stockholmshem",
+		"phase": "Norra 2",
+		"lat": 59.35886237,
+		"lng": 18.09030331
 	},
 	{
-	  "building": "Sonfjallet 3",
-	  "developer": "Erik Wallin",
-	  "phase": "Norra 2",
-	  "lat": 59.35942835,
-	  "lng": 18.09093778
+		"building": "Sonfjallet 3",
+		"developer": "Erik Wallin",
+		"phase": "Norra 2",
+		"lat": 59.35942835,
+		"lng": 18.09093778
 	},
 	{
-	  "building": "Sonfjallet 1",
-	  "developer": "HEBA",
-	  "phase": "Norra 2",
-	  "lat": 59.35901409,
-	  "lng": 18.09158018
+		"building": "Sonfjallet 1",
+		"developer": "HEBA",
+		"phase": "Norra 2",
+		"lat": 59.35901409,
+		"lng": 18.09158018
 	},
 	{
-	  "building": "Sonfjallet 2",
-	  "developer": "Skanska",
-	  "phase": "Norra 2",
-	  "lat": 59.35942312,
-	  "lng": 18.09199974
+		"building": "Sonfjallet 2",
+		"developer": "Skanska",
+		"phase": "Norra 2",
+		"lat": 59.35942312,
+		"lng": 18.09199974
 	},
 	{
-	  "building": "Casabanca 1",
-	  "developer": "Stockholms Hamnar",
-	  "phase": "Vartahamnen",
-	  "lat": 59.35135299,
-	  "lng": 18.1117903
+		"building": "Casabanca 1",
+		"developer": "Stockholms Hamnar",
+		"phase": "Vartahamnen",
+		"lat": 59.35135299,
+		"lng": 18.1117903
 	},
 	{
-	  "building": "Backakra 4",
-	  "developer": "Besqab",
-	  "phase": "Brofastet",
-	  "lat": 59.35992555,
-	  "lng": 18.09486179
+		"building": "Backakra 4",
+		"developer": "Besqab",
+		"phase": "Brofastet",
+		"lat": 59.35992555,
+		"lng": 18.09486179
 	},
 	{
-	  "building": "Backakra 7",
-	  "developer": "Oscar Properties",
-	  "phase": "Brofastet",
-	  "lat": 59.35972129,
-	  "lng": 18.09431265
+		"building": "Backakra 7",
+		"developer": "Oscar Properties",
+		"phase": "Brofastet",
+		"lat": 59.35972129,
+		"lng": 18.09431265
 	},
 	{
-	  "building": "Backakra 5; 6",
-	  "developer": "ake Sundvall",
-	  "phase": "Brofastet",
-	  "lat": 59.35953958,
-	  "lng": 18.09484313
+		"building": "Backakra 5; 6",
+		"developer": "ake Sundvall",
+		"phase": "Brofastet",
+		"lat": 59.35953958,
+		"lng": 18.09484313
 	},
 	{
-	  "building": "Backakra 2",
-	  "developer": "Stockholmshem",
-	  "phase": "Brofastet",
-	  "lat": 59.36017779,
-	  "lng": 18.09363346
+		"building": "Backakra 2",
+		"developer": "Stockholmshem",
+		"phase": "Brofastet",
+		"lat": 59.36017779,
+		"lng": 18.09363346
 	},
 	{
-	  "building": "Backakra 1",
-	  "developer": "Tobin Properties",
-	  "phase": "Brofastet",
-	  "lat": 59.35959383,
-	  "lng": 18.0930446
+		"building": "Backakra 1",
+		"developer": "Tobin Properties",
+		"phase": "Brofastet",
+		"lat": 59.35959383,
+		"lng": 18.0930446
 	},
 	{
-	  "building": "Backakra 8",
-	  "developer": "Einar Mattsson 1",
-	  "phase": "Brofastet",
-	  "lat": 59.35963099,
-	  "lng": 18.09380778
+		"building": "Backakra 8",
+		"developer": "Einar Mattsson 1",
+		"phase": "Brofastet",
+		"lat": 59.35963099,
+		"lng": 18.09380778
 	},
 	{
-	  "building": "Koppangen 3",
-	  "developer": "HSB",
-	  "phase": "Brofastet",
-	  "lat": 59.36039062,
-	  "lng": 18.09598136
+		"building": "Koppangen 3",
+		"developer": "HSB",
+		"phase": "Brofastet",
+		"lat": 59.36039062,
+		"lng": 18.09598136
 	},
 	{
-	  "building": "Koppangen 1",
-	  "developer": "Riksbyggen",
-	  "phase": "Brofastet",
-	  "lat": 59.36025284,
-	  "lng": 18.09534799
+		"building": "Koppangen 1",
+		"developer": "Riksbyggen",
+		"phase": "Brofastet",
+		"lat": 59.36025284,
+		"lng": 18.09534799
 	},
 	{
-	  "building": "Koppangen 4",
-	  "developer": "Einar Mattsson 2",
-	  "phase": "Brofastet",
-	  "lat": 59.35973883,
-	  "lng": 18.09610084
+		"building": "Koppangen 4",
+		"developer": "Einar Mattsson 2",
+		"phase": "Brofastet",
+		"lat": 59.35973883,
+		"lng": 18.09610084
 	},
 	{
-	  "building": "Hjorthagens idrottsplats 2",
-	  "developer": "Fastighetskontoret",
-	  "phase": "Gasverket",
-	  "lat": 59.35700032,
-	  "lng": 18.09330298
+		"building": "Hjorthagens idrottsplats 2",
+		"developer": "Fastighetskontoret",
+		"phase": "Gasverket",
+		"lat": 59.35700032,
+		"lng": 18.09330298
 	},
 	{
-	  "building": "Gasklockorna 4",
-	  "developer": "SISAB skola",
-	  "phase": "Gasverket",
-	  "lat": 59.35755709,
-	  "lng": 18.09299683
+		"building": "Gasklockorna 4",
+		"developer": "SISAB skola",
+		"phase": "Gasverket",
+		"lat": 59.35755709,
+		"lng": 18.09299683
 	},
 	{
-	  "building": "Hjorthagens idrottsplats 1",
-	  "developer": "SISAB forskola",
-	  "phase": "Gasverket",
-	  "lat": 59.35662477,
-	  "lng": 18.09250025
+		"building": "Hjorthagens idrottsplats 1",
+		"developer": "SISAB forskola",
+		"phase": "Gasverket",
+		"lat": 59.35662477,
+		"lng": 18.09250025
 	},
 	{
-	  "building": "Stalklockan 1",
-	  "developer": "Oscar Properties",
-	  "phase": "Gasklocka",
-	  "lat": 59.35568026,
-	  "lng": 18.09019641
+		"building": "Stalklockan 1",
+		"developer": "Oscar Properties",
+		"phase": "Gasklocka",
+		"lat": 59.35568026,
+		"lng": 18.09019641
 	}
-  ]
+]
